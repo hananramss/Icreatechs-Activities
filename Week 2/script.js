9f86c0ea12fc67513f73d3b32c9f7b0399c17088
hananramss <hananramos19@gmail.com>
2023-10-03 17:05:36 +0800

    Added Changes to portfolio.html

--- Week 2/script.js
+++ Week 2/script.js
@@ -1,14 +1,6 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click",() => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-
-})
-
-document.querySelectorAll(".nav-link").forEach(n => addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-
-}))
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
